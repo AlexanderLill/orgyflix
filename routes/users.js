85938ab4f32159a1b27d45f1172ca96e7ca06b4b
Lorenzo Donini <lorenzo.donini@tum.de>
2016-09-12 18:10:17 +0200

    Add router api calls

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var jwt = require('express-jwt');
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
+
+var userController = require('../controllers/UserController');
+
+/*router.get('/:user/invitations', auth, function(req, res, next) {
+  //TODO: implement
 });
+
+router.get('/:user', auth, function(req, res, next) {
+
+});*/
+
+//Registration & Login
+router.post('/register', userController.registerUser);
+
+router.post('/login', userController.login);
+
+//Parameters
+router.param('user', function(req, res, next, id) {
+    var query = User.findById(id);
+
+    query.exec(function (err, user){
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return next(new Error("can't find user"));
+        }
+
+        req.user = user;
+        return next();
+    });
+});
+
+//Getting Single User
+router.get('/', auth, userController.getUsers);
+
+router.get('/:user', auth, function(req, res, next) {
+    res.json(req.user);
+});
+
+//router.get('/:user', userController.login);
 
 /*
   router.put('/:id/invitations'
