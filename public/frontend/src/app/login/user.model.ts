1e00bbcd911c47188e408ab1e1d7559d4d8fad03
Lorenzo Donini <lorenzo.donini@tum.de>
2016-09-14 09:02:14 +0200

    Update user model

--- public/frontend/src/app/login/user.model.ts
+++ public/frontend/src/app/login/user.model.ts
@@ -1,5 +1,6 @@
 export class User {
-    constructor(public username: string = '',
-                public password: string = '') {
-    }
+    public username: string = '';
+    public password: string = '';
+    public firstname: string = '';
+    public lastname: string = '';
 }
