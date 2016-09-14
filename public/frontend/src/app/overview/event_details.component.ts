5e9879faf8b1b0fcf1c4e92e29988cb4ec859b1b
Lorenzo Donini <lorenzo.donini@tum.de>
2016-09-14 14:10:47 +0200

    Implement event details inside overview

---
+++ public/frontend/src/app/overview/event_details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Event } from '../models/event';
+import { ApiService } from "../services/api.service";
+import {Subscription} from "rxjs";
+
+@Component({
+    selector: 'event-details',
+    templateUrl: 'app/event_details/event_details.html',
+})
+export class EventDetailsComponent implements OnInit, OnDestroy  {
+    public event: Event;
+    private sub: Subscription;
+
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private service: ApiService) {}
+
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            let id = +params['id']; // (+) converts string 'id' to a number
+            this.service.getEvent(id).subscribe(result => this.event = result, error => this.event = null);
+        });
+    }
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    gotToEvents() {
+        this.router.navigate(['../overview']);
+    }
+}
